fa611ce57138eb7f002627a01e90fd5b5f9d7187
Ashish Bhosle <asbhosle2003@gmail.com>
2024-06-13 19:31:33 +0530

    done with workflow pg components

---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { EditorCanvasCardType, EditorNodeType } from '@/lib/types'
+import { useEditor } from '@/providers/editor-provider'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import ReactFlow, {
+  Background,
+  Connection,
+  Controls,
+  Edge,
+  EdgeChange,
+  MiniMap,
+  NodeChange,
+  ReactFlowInstance,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+} from 'reactflow'
+import 'reactflow/dist/style.css'
+import EditorCanvasCardSingle from './editor-canvas-card-single'
+
+type Props = {}
+
+const initialNodes: EditorNodeType[] = []
+
+const initialEdges: { id: string; source: string; target: string }[] = []
+
+const EditorCanvas = (props: Props) => {
+    const { dispatch, state } = useEditor()
+
+const nodeTypes = useMemo(
+    () => ({
+      Action: EditorCanvasCardSingle,
+      Trigger: EditorCanvasCardSingle,
+      Email: EditorCanvasCardSingle,
+      Condition: EditorCanvasCardSingle,
+      AI: EditorCanvasCardSingle,
+      Slack: EditorCanvasCardSingle,
+      'Google Drive': EditorCanvasCardSingle,
+      Notion: EditorCanvasCardSingle,
+      Discord: EditorCanvasCardSingle,
+      'Custom Webhook': EditorCanvasCardSingle,
+      'Google Calendar': EditorCanvasCardSingle,
+      Wait: EditorCanvasCardSingle,
+    }),
+    []
+  )
+  return <div>EditorCanvas </div>
+}
+
+export default EditorCanvas 
