4d43aaab9c42c13d60f6f0ba1c453bb4be0e0018
Ashish Bhosle <asbhosle2003@gmail.com>
2024-06-18 23:10:33 +0530

    git push

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ export default authMiddleware({
   publicRoutes: [
     '/',
     '/api/clerk-webhook',
+    '/api/drive-activity',
     '/api/drive-activity/notification',
     '/api/payment/success',
   ],
@@ -19,9 +20,3 @@ export default authMiddleware({
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 }
-
-// https://www.googleapis.com/auth/userinfo.email
-// https://www.googleapis.com/auth/userinfo.profile
-// https://www.googleapis.com/auth/drive.activity.readonly
-// https://www.googleapis.com/auth/drive.metadata
-// https://www.googleapis.com/auth/drive.readonly
